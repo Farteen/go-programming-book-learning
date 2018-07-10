3bd26043b4d01800ea2cc6770167e147c0556b93
Xu Jiawei <jiawei.xu@centrixlink.com>
2018-07-10 11:01:20 +0800

    add 2.2

--- 1.7/main.go
+++ 1.7/main.go
@@ -1,16 +1,86 @@
 package main
 
 import (
+	"math"
+	"image"
+	"image/gif"
+	"strconv"
+	"sync"
 	"fmt"
 	"log"
 	"net/http"
+	"image/color"
+	"math/rand"
+)
+
+var mu sync.Mutex
+var count int 
+var palette = []color.Color{color.White, color.Black}
+const (
+	whiteIndex = 0
+	blackIndex = 1
 )
 
 func main() {
-	http.HandleFunc("/", handler) 
+	http.HandleFunc("/", lissajour)
+	http.HandleFunc("/count", counter)
 	log.Fatal(http.ListenAndServe("localhost:8889", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "URLPath = %q\n", r.URL.Path)
+	// mu.Lock()
+	// count++
+	// mu.Unlock()
+	// fmt.Fprintf(w, "URLPath = %q\n", r.URL.Path)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n",count)
+	mu.Unlock()
+}
+
+func lissajour(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		log.Print(err);
+	}
+	cycles, err := strconv.ParseFloat(r.Form["cycles"][0], 32)
+	fmt.Println(cycles)
+	if err != nil {
+		return
+	}
+	const (
+		res = 0.001
+		size = 100
+		nframes = 64
+		delay = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t* freq+phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(w, &anim)
 }
---
+++ 2.2/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const freezingF, boilingF = 32.0, 212.0
+	fmt.Printf("%g", freezingF)
+}
